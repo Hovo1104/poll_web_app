4c12ba170d8b699c9a97c90f199e0ddbc87a01ed
Hovo1104 <hovhannes.abrahamyan.1995@mail.ru>
2022-04-10 15:14:36 +0400

    Poll_web_app

---
+++ poll_system/src/main/java/com/epam/db/conf/DBConnectionProvider.java
@@ -0,0 +1,46 @@
+package com.epam.db.conf;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnectionProvider {
+
+    private volatile static DBConnectionProvider instance;
+    private  Connection connection;
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/pall_with_spring";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root";
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    private DBConnectionProvider(){
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    public static DBConnectionProvider getInstance(){
+        if (instance == null){
+            synchronized (DBConnectionProvider.class){
+                if (instance == null){
+                    instance = new DBConnectionProvider();
+                }
+            }
+        }
+        return instance;
+
+    }
+    public  Connection getConnection(){
+        try {
+            if (connection == null || connection.isClosed()){
+                connection = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
